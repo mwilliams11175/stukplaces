130d609e2732f538e5e3ab112943abe0304ba6be
Michael Williams <mwilliams11175@gmail.com>
2016-01-16 14:31:50 -0600

    added devise

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,15 @@
+class RegistrationsController < Devise::RegistrationsController
+
+	private
+
+		def sign_up_params
+			params.required(:user).permit(:name,:email, :password)
+		end
+
+
+		def account_update_params
+			params.require(:user).permit(:name, :email, :password, :current_password)
+		end
+
+	
+end
---
+++ db/migrate/20160116195538_add_nameto_user2.rb
@@ -0,0 +1,4 @@
+class AddNametoUser2 < ActiveRecord::Migration
+  def change
+  end
+end
